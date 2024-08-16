2612dc8312e984303394ccb26985fc5c4fdabb9e
ok-pennywise <souryadeeptasaha03@gmail.com>
2024-08-16 22:37:07 +0530

    B

--- main.go
+++ main.go
@@ -1,33 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
-var socketDirector = NewSocketDirector()
+var socketDirector = NewSocketManager()
+
+var secretKey string
+
+func verifyKey(keyString string) (*jwt.Token, error) {
+	key, err := jwt.Parse(keyString, func(k *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !key.Valid {
+		return nil, fmt.Errorf("Invalid token")
+	}
+
+	return key, nil
+}
+
+func generateRandomID() string {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate a random integer
+	randomInt := rand.Intn(1000000) // Generates a random integer between 0 and 999999
+
+	// Convert the integer to a string
+	randomID := strconv.Itoa(randomInt)
+
+	return randomID
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
-
-	if 1 == 1{
-		http.Error(w, "Not authorized", http.StatusUnauthorized)
-		return
-	}
 
 	c, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
-		
+
 		log.Print("Upgrade error:", err)
 		return
 	}
 	defer c.Close()
 
-	socketDirector.Track("sf", c)
-	defer socketDirector.Untrack("sf", c)
+	connId :=  "632913"
+	socketDirector.Track("sf", connId,c)
+	defer socketDirector.Untrack("sf", connId, c)
 
 	for {
 		_, _, err := c.ReadMessage()
@@ -41,4 +73,5 @@ func echo(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", echo)
 	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
--- socketdirector.go
+++ socketdirector.go
@@ -9,68 +9,81 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type SocketDirector struct {
-	redisConn       *redis.Client
-	mx              sync.Mutex
-	sockets         map[string][]*websocket.Conn
-	interComm       chan *redis.Message
+type SocketManager struct {
+	redisConn *redis.Client
+	mx        sync.Mutex
+
+	sockets         map[string]map[string][]*websocket.Conn
 	channelContexts map[string]*context.CancelFunc
+
+	interComm chan *redis.Message
 }
 
-func NewSocketDirector() *SocketDirector {
-
-	sd := &SocketDirector{
-		sockets:         make(map[string][]*websocket.Conn),
+func NewSocketManager() *SocketManager {
+	sd := &SocketManager{
 		redisConn:       redis.NewClient(&redis.Options{Addr: "localhost:6379"}),
-		interComm:       make(chan *redis.Message, 1024),
+		sockets:         make(map[string]map[string][]*websocket.Conn),
 		channelContexts: make(map[string]*context.CancelFunc),
+		interComm:       make(chan *redis.Message, 1024),
 	}
-
-	// Start message broadcasting goroutine
 	sd.startBroadcasting()
-
 	return sd
 }
 
-func (sd *SocketDirector) Track(channel string, conn *websocket.Conn) {
+func (sd *SocketManager) Track(channel, connId string, conn *websocket.Conn) {
 	sd.mx.Lock()
 	defer sd.mx.Unlock()
 
-	// Add the new WebSocket connection to the channel
-	sd.sockets[channel] = append(sd.sockets[channel], conn)
+	if _, ok := sd.sockets[channel]; !ok {
+		sd.sockets[channel] = make(map[string][]*websocket.Conn)
+	}
 
-	// Subscribe to Redis channel if not already subscribed
+	sd.sockets[channel][connId] = append(sd.sockets[channel][connId], conn)
+
 	if _, ok := sd.channelContexts[channel]; !ok {
 		subscriber := sd.redisConn.Subscribe(context.Background(), channel)
 		ctx, cancel := context.WithCancel(context.Background())
 		sd.channelContexts[channel] = &cancel
 		go sd.readMessage(ctx, subscriber)
 	}
+
+	log.Println(sd.sockets)
 }
 
-func (sd *SocketDirector) Untrack(channel string, conn *websocket.Conn) {
+func (sd *SocketManager) Untrack(channel, connId string, conn *websocket.Conn) {
 	sd.mx.Lock()
 	defer sd.mx.Unlock()
 
-	// Remove the WebSocket connection from the channel
-	for i, socket := range sd.sockets[channel] {
-		if socket == conn {
-			sd.sockets[channel] = append(sd.sockets[channel][:i], sd.sockets[channel][i+1:]...)
-			break
+	if socketsByChannel, ok := sd.sockets[channel]; ok {
+		// Retrieve the slice of connections for the given connId
+		sockets := socketsByChannel[connId]
+
+		// Create a new slice to hold the remaining connections
+		// Iterate over the existing slice and append only the connections that do not match the one being removed
+		for i, socket := range sockets {
+			if socket == conn {
+				sd.sockets[channel][connId] = append(sockets[:i], sockets[i+1:]...)
+			}
+		}
+
+		if len(sd.sockets[channel][connId]) == 0 {
+			delete(sd.sockets[channel], connId)
+		}
+
+		// Clean up the channel if there are no connections left
+		if len(sd.sockets[channel]) == 0 {
+			delete(sd.sockets, channel)
+			if cancelFunc, ok := sd.channelContexts[channel]; ok {
+				(*cancelFunc)()
+				delete(sd.channelContexts, channel)
+			}
 		}
 	}
 
-	// Cleanup channel if there are no more connections
-	if len(sd.sockets[channel]) == 0 {
-		delete(sd.sockets, channel)
-		if cancelFunc, ok := sd.channelContexts[channel]; ok {
-			(*cancelFunc)()
-			delete(sd.channelContexts, channel)
-		}
-	}
+	log.Println(sd.sockets)
 }
 
-func (sd *SocketDirector) readMessage(ctx context.Context, subscriber *redis.PubSub) {
+func (sd *SocketManager) readMessage(ctx context.Context, subscriber *redis.PubSub) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -87,21 +100,26 @@ func (sd *SocketDirector) readMessage(ctx context.Context, subscriber *redis.Pub
 	}
 }
 
-func (sd *SocketDirector) broadcastMessage() {
+func (sd *SocketManager) broadcastMessage() {
 	for message := range sd.interComm {
 		sd.mx.Lock()
-		for _, socket := range sd.sockets[message.Channel] {
-			err := socket.WriteMessage(websocket.TextMessage, []byte(message.Payload))
-			if err != nil {
-				log.Println("Error broadcasting message:", err)
-				socket.Close()
-				// sd.Untrack(message.Channel, socket)
+		for _, sockets := range sd.sockets[message.Channel] {
+			if len(sockets) > 1 {
+				for _, socket := range sockets {
+					err := socket.WriteMessage(websocket.TextMessage, []byte(message.Payload))
+					if err != nil {
+						log.Println("Error sending message:", err)
+					}
+				}
+			} else {
+				sockets[0].WriteMessage(websocket.TextMessage, []byte(message.Payload))
 			}
+
 		}
 		sd.mx.Unlock()
 	}
 }
 
-func (sd *SocketDirector) startBroadcasting() {
+func (sd *SocketManager) startBroadcasting() {
 	go sd.broadcastMessage()
 }
